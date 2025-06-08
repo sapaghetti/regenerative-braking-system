68b4123284a28c5ca6e2f8e9182e8b47a9d245b5
WalWook <chamkhs3@gmail.com>
2025-06-08 18:07:30 +0900

    feat: make_bin_file 수정 [CAN-75]

--- OTA_Server/make_bin_file.py
+++ OTA_Server/make_bin_file.py
@@ -1,2 +1,6 @@
+ecu_id = 3
+version = 2
+
 with open("Sapaghetti.bin", "wb") as f:
-    f.write(bytes([2]))  # 버전 2
+    f.write(f"{ecu_id}\n".encode())      # 첫 줄: ECU ID (ex: "1\n")
+    f.write(f"{version}\n".encode())     # 두 번째 줄: 버전 (ex: "2\n")
