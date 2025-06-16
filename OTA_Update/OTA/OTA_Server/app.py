8811347c15232e05ba3fe4840a3d40eadb4c468f
WalWook <chamkhs3@gmail.com>
2025-06-16 16:42:57 +0900

    feat: app.py 수정 [CAN-75]

--- OTA_Update/OTA/OTA_Server/app.py
+++ OTA_Update/OTA/OTA_Server/app.py
@@ -239,5 +239,5 @@ def report_versions():
         write_audit_log(f"[VERSION REPORT FAIL] {str(e)}")
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+#if __name__ == "__main__":
+    #app.run(host='0.0.0.0', port=5000, debug=True)
